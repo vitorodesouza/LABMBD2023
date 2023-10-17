34442fdbf61689ad555269fcf98c628c6841c236
Miguel Galan <galan.miguelc@gmail.com>
2023-10-17 19:26:43 +0200

    actualizar a multi-stage

--- SESION2/volumen/Main.java
+++ SESION2/volumen/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Hellow Vitor");
+        System.out.println("Hello Vitor");
         System.out.println(); 
     }
 }
